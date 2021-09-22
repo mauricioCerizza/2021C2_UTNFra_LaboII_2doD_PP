112802923d30ba071f81c066b9679e7770079afa
Lau Carelli <carelli.lc@gmail.com>
2021-09-21 22:27:59 -0300

    Se sube el manejo de computadores

---
+++ cyber-2doD-PrimerParcial/Biblioteca/Computadora/Computadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca
+{
+    public class Computadora
+    {
+        private string identificador;
+        private List<Software> software;
+        private List<Periferico> perifericos;
+        private List<Juego> juegos;
+        private EspecificacionHardware especificacion;
+
+        public Computadora(string identificador, List<Software> software, List<Periferico> perifericos, List<Juego> juegos, EspecificacionHardware especificacion)
+        {
+            this.identificador = identificador;
+            this.software = software;
+            this.perifericos = perifericos;
+            this.juegos = juegos;
+            this.especificacion = especificacion;
+        }
+    }
+}
